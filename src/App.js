407e7da89ae70abd120cee5d8d24338be987d534
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2018-01-27 00:27:01 +0100

    App initialization

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './css/App.css';
+//import "./css/icon-font.css";
+
+class App extends Component {
+  render() {
+    return (
+        <header className="header">
+          Some text..
+        </header>
+    );
+  }
+}
+
+export default App;
