7cc98450f063734b158126d1369a023b1a875e55
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2018-01-27 06:26:00 +0100

    Button and Button animations

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,22 +1,24 @@
-import React,{ Component } from 'react';
+import React, { Component } from 'react';
 
 class Header extends Component {
-    render() {
-        return (
-            <header className="header">
-                <div className="logo-box">
-                    <img  src="../img/logo-white.png" alt="logo" className="logo"/>
-                </div>
-                <div className="text-box">
-                    <h1 className="heading-primary">
-                        <span className="heading-primary-main">Outdoors</span>
-                        <span className="heading-primary-sub">Is where Life happens</span>
-                    </h1>
-                </div>
-        
-            </header>
-        )
-    }
+	render() {
+		return (
+			<header className="header">
+				<div className="logo-box">
+					<img src="../img/logo-white.png" alt="logo" className="logo" />
+				</div>
+				<div className="text-box">
+					<h1 className="heading-primary">
+						<span className="heading-primary-main">Outdoors</span>
+						<span className="heading-primary-sub">Is where Life happens</span>
+					</h1>
+					<a href="#" className="btn btn-white">
+						Discover Our Tours
+					</a>
+				</div>
+			</header>
+		);
+	}
 }
 
 export default Header;
