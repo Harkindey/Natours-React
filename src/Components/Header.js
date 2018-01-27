8224c5748484888dc8fef050bbce8c58638233bf
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2018-01-27 17:10:14 +0100

    implementing BEM

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,15 +4,19 @@ class Header extends Component {
 	render() {
 		return (
 			<header className="header">
-				<div className="logo-box">
-					<img src="../img/logo-white.png" alt="logo" className="logo" />
+				<div className="header__logo-box">
+					<img
+						src="../img/logo-white.png"
+						alt="logo"
+						className="header__logo"
+					/>
 				</div>
-				<div className="text-box">
+				<div className="header__text-box">
 					<h1 className="heading-primary">
-						<span className="heading-primary-main">Outdoors</span>
-						<span className="heading-primary-sub">Is where Life happens</span>
+						<span className="heading-primary--main">Outdoors</span>
+						<span className="heading-primary--sub">Is where Life happens</span>
 					</h1>
-					<a href="#" className="btn btn-white btn-animated">
+					<a href="#" className="btn btn--white btn--animated">
 						Discover Our Tours
 					</a>
 				</div>
