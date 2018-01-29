ccfaf96ccbe6e5313071a160e5f7789527314314
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2018-01-29 21:47:14 +0100

    Build grid template remove `.scss` from import statement

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 import './sass/main.scss';
 //import "./css/icon-font.css";
 import Header from './Components/Header';
+import GridTest from './Components/Grid-Test';
 
 class App extends Component {
 	render() {
 		return (
 			<div>
 				<Header />
+				<GridTest />
 			</div>
 		);
 	}
---
+++ src/Components/Grid-Test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const GridTest = () => {
+	return (
+		<section className="gridtest">
+			<div className="row">
+				<div className="col-1-2">Col 1 of 2</div>
+				<div className="col-1-2">Col 1 of 2</div>
+			</div>
+			<div className="row">
+				<div className="col-1-3">Col 1 of 3</div>
+				<div className="col-1-3">Col 1 of 3</div>
+				<div className="col-1-3">Col 1 of 3</div>
+			</div>
+			<div className="row">
+				<div className="col-1-3">Col 1 of 3</div>
+				<div className="col-2-3">Col 2 of 3</div>
+			</div>
+			<div className="row">
+				<div className="col-1-4">Col 1 of 4</div>
+				<div className="col-1-4">Col 1 of 4</div>
+				<div className="col-1-4">Col 1 of 4</div>
+				<div className="col-1-4">Col 1 of 4</div>
+			</div>
+			<div className="row">
+				<div className="col-1-4">Col 1 of 4</div>
+				<div className="col-1-4">Col 1 of 4</div>
+				<div className="col-2-4">Col 2 of 4</div>
+			</div>
+			<div className="row">
+				<div className="col-1-4">Col 1 of 4</div>
+				<div className="col-3-4">Col 3 of 4</div>
+			</div>
+		</section>
+	);
+};
+
+export default GridTest;
