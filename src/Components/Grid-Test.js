4357402063a4e93781c220bd0a7505fb6ecaf34a
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2018-01-29 23:45:13 +0100

    Built custom grid system

--- src/Components/Grid-Test.js
+++ src/Components/Grid-Test.js
@@ -4,32 +4,32 @@ const GridTest = () => {
 	return (
 		<section className="gridtest">
 			<div className="row">
-				<div className="col-1-2">Col 1 of 2</div>
-				<div className="col-1-2">Col 1 of 2</div>
+				<div className="col-1-of-2">Col 1 of 2</div>
+				<div className="col-1-of-2">Col 1 of 2</div>
 			</div>
 			<div className="row">
-				<div className="col-1-3">Col 1 of 3</div>
-				<div className="col-1-3">Col 1 of 3</div>
-				<div className="col-1-3">Col 1 of 3</div>
+				<div className="col-1-of-3">Col 1 of 3</div>
+				<div className="col-1-of-3">Col 1 of 3</div>
+				<div className="col-1-of-3">Col 1 of 3</div>
 			</div>
 			<div className="row">
-				<div className="col-1-3">Col 1 of 3</div>
-				<div className="col-2-3">Col 2 of 3</div>
+				<div className="col-1-of-3">Col 1 of 3</div>
+				<div className="col-2-of-3">Col 2 of 3</div>
 			</div>
 			<div className="row">
-				<div className="col-1-4">Col 1 of 4</div>
-				<div className="col-1-4">Col 1 of 4</div>
-				<div className="col-1-4">Col 1 of 4</div>
-				<div className="col-1-4">Col 1 of 4</div>
+				<div className="col-1-of-4">Col 1 of 4</div>
+				<div className="col-1-of-4">Col 1 of 4</div>
+				<div className="col-1-of-4">Col 1 of 4</div>
+				<div className="col-1-of-4">Col 1 of 4</div>
 			</div>
 			<div className="row">
-				<div className="col-1-4">Col 1 of 4</div>
-				<div className="col-1-4">Col 1 of 4</div>
-				<div className="col-2-4">Col 2 of 4</div>
+				<div className="col-1-of-4">Col 1 of 4</div>
+				<div className="col-1-of-4">Col 1 of 4</div>
+				<div className="col-2-of-4">Col 2 of 4</div>
 			</div>
 			<div className="row">
-				<div className="col-1-4">Col 1 of 4</div>
-				<div className="col-3-4">Col 3 of 4</div>
+				<div className="col-1-of-4">Col 1 of 4</div>
+				<div className="col-3-of-4">Col 3 of 4</div>
 			</div>
 		</section>
 	);
