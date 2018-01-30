c5fd390ac161790c3e8b67da45ef83f0da0f6f28
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2018-01-30 09:39:29 +0100

    About page  - contents  - button styling  - button animation

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,13 +4,33 @@ class About extends Component {
 	render() {
 		return (
 			<section className="section-about">
-				<div className="u-center-text u-margin-bottom-8">
+				<div className="u-center-text u-margin-bottom-big">
 					<h2 className="heading-secondary">
 						Exciting tours or advenurous people
 					</h2>
 				</div>
 				<div className="row">
-					<div className="col-1-of-2">Text</div>
+					<div className="col-1-of-2">
+						<h3 className="heading-tertiary u-margin-bottom-small">
+							You're Going to fall in love with nature
+						</h3>
+						<p className="paragraph">
+							Lorem Ipsum is simply dummy text of the printing and typesetting
+							industry. Lorem Ipsum has been the industry's standard dummy text
+							ever since the 1500s, when an unknown printer took a galley of
+							type and scrambled it to make a type specimen book
+						</p>
+						<h3 className="heading-tertiary u-margin-bottom-small">
+							Live Adventures like you never have before
+						</h3>
+						<p className="paragraph">
+							Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+							accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+						</p>
+						<a href="#" className="btn-text">
+							Learn More &rarr;
+						</a>
+					</div>
 					<div className="col-1-of-2">Image Composition</div>
 				</div>
 			</section>
