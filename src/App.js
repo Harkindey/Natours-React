57191fda75c63c32697cac1e87e075aa316e73d6
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2018-01-30 08:34:55 +0100

    About Component  - Utility styling  - text backgroud (linear - gradient)

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import './sass/main.scss';
 //import "./css/icon-font.css";
 import Header from './Components/Header';
 import GridTest from './Components/Grid-Test';
+import About from './Components/About';
 
 class App extends Component {
 	render() {
 		return (
 			<div>
 				<Header />
-				<GridTest />
+				<main>
+					<About />
+				</main>
+				{/* <GridTest /> */}
 			</div>
 		);
 	}
---
+++ src/Components/About.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+	render() {
+		return (
+			<section className="section-about">
+				<div className="u-center-text u-margin-bottom-8">
+					<h2 className="heading-secondary">
+						Exciting tours or advenurous people
+					</h2>
+				</div>
+				<div className="row">
+					<div className="col-1-of-2">Text</div>
+					<div className="col-1-of-2">Image Composition</div>
+				</div>
+			</section>
+		);
+	}
+}
+
+export default About;
