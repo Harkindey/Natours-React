55d8f29ab068279a8e04d23d32711161a9434530
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2018-05-15 15:10:18 +0100

    The stories component begin

--- src/Components/Stories.js
+++ src/Components/Stories.js
@@ -11,8 +11,32 @@ class Stories extends Component {
                 </div>
                 <div className="row">
                     <div className="story">
-                        TEXT
+                        <figure className="story__shape">
+                            <img src="img/nat-8.jpg" alt="person on a tour" className="story__img" />
+                            <figcaption className="story__caption">Mary Smith</figcaption>
+                        </figure>
+                        <div className="story__text">
+                            <h3 className="heading-tertiary u-margin-bottom-small">I had the best week ever with my family</h3>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        </div>
                     </div>
+                </div>
+                <div className="row">
+                    <div className="story">
+                        <figure className="story__shape">
+                            <img src="img/nat-9.jpg" alt="person on a tour" className="story__img" />
+                            <figcaption className="story__caption">Jack Wilson</figcaption>
+                        </figure>
+                        <div className="story__text">
+                            <h3 className="heading-tertiary u-margin-bottom-small">WOW! My life is completely different now</h3>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="u-center-text u-margin-top-huge">
+                    <a href="#" className="btn-text">
+                        Read all stories &rarr;
+					</a>
                 </div>
             </section>
         )
