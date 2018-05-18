73c45fbbfb6269e9747a4c575fe9e26c242fbd6d
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2018-05-18 16:46:37 +0100

    Creating custom radio buttons with css

--- src/Components/Booking.js
+++ src/Components/Booking.js
@@ -19,11 +19,31 @@ class Booking extends Component {
                                     <input type="email" className="form__input" id="email" placeholder="Email" required />
                                     <label htmlFor="email" className="form__label">Email</label>
                                 </div>
+                                <div className="form__group .u-margin-bottom-medium">
+                                    <div className="form__radio-group">
+                                        <input type="radio" className="form__radio-input" id="small" name="size" />
+                                        <label htmlFor="small" className="form__radio-label">
+                                            <span className="form__radio-button"></span>
+                                            Small tour group
+                                        </label>
+                                    </div>
+
+                                    <div className="form__radio-group">
+                                        <input type="radio" className="form__radio-input" id="large" name="size" />
+                                        <label htmlFor="large" className="form__radio-label">
+                                            <span className="form__radio-button"></span>
+                                            Large tour group
+                                        </label>
+                                    </div>
+                                </div>
+                                <div className="form__group">
+                                    <button className="btn btn--green btn-green">Next Step &rarr;</button>
+                                </div>
                             </form>
                         </div>
                     </div>
                 </div>
-            </section>
+            </section >
         )
     }
 };
