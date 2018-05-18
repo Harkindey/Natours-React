0ef66e1870ab3cb5d7d965b505d0631ded64b1e7
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2018-05-18 03:07:27 +0100

    css background video

--- src/Components/Stories.js
+++ src/Components/Stories.js
@@ -4,6 +4,13 @@ class Stories extends Component {
     render() {
         return (
             <section className="section-stories">
+                <div className="bg-video">
+                    <video className="bg-video__content" autoPlay muted loop>
+                        <source src="img/video.mp4" type="video/mp4" />
+                        <source src="img/video.webm" type="video/webm" />
+                        Your browser is not supported!
+                    </video>
+                </div>
                 <div className="u-center-text u-margin-bottom-big">
                     <h2 className="heading-secondary">
                         We Make people genuiuely happy
