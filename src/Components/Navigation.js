34bd9fd72176e2e5eb90b21e9011be1b00a2c6c0
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2018-05-20 19:17:10 +0100

    - transform-origin

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -6,7 +6,9 @@ class Navigation extends Component {
             <div className="navigation">
                 <input type="checkbox" className="navigation__checkbox" id="navi-toggle" />
 
-                <label htmlFor="navi-toggle" className="navigation__button">MENU</label>
+                <label htmlFor="navi-toggle" className="navigation__button">
+                    <span className="navigation__icon">&nbsp;</span>
+                </label>
 
                 <div className="navigation__background">&nbsp;</div>
 
