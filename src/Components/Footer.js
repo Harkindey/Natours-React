8ca21920f56d52914cf37450437b6cc61a4badb7
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2020-01-16 12:04:30 +0100

    Density Switching

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,32 +1,68 @@
 import React, { Component } from 'react';
 
 class Footer extends Component {
-    render() {
-        return (
-            <footer className="footer">
-                <div className="footer__logo-box">
-                    <img src="img/logo-green-2x.png" alt="full logo" className="footer__logo" />
-                </div>
-                <div className="row">
-                    <div className="col-1-of-2">
-                        <div className="footer__navigation">
-                            <ul className="footer__list">
-                                <li className="footer__item"><a href="#" className="footer__link">Company</a></li>
-                                <li className="footer__item"><a href="#" className="footer__link">Contact us</a></li>
-                                <li className="footer__item"><a href="#" className="footer__link">Careers</a></li>
-                                <li className="footer__item"><a href="#" className="footer__link">Privacy Policy</a></li>
-                                <li className="footer__item"><a href="#" className="footer__link">Terms</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="col-1-of-2">
-                        <p className="footer__copyright">
-                            Built by <a href="#" className="footer__link">Akinde Emmanuel</a> on a online course <a href="#" className="footer__link">Advanced CSS and Sass</a> Copyright &copy; Jonas.io. Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse vel itaque, placeat obcaecati repudiandae eum fugiat autem quae vitae eius.
-                        </p>
-                    </div>
-                </div>
-            </footer>
-        )
-    }
-};
-export default Footer
+	render() {
+		return (
+			<footer className="footer">
+				<div className="footer__logo-box">
+					<img
+						// src="img/logo-green-2x.png"
+						srcset="img/logo-green-1x.png 1x, img/logo-green-2x.png 2x"
+						alt="full logo"
+						className="footer__logo"
+					/>
+				</div>
+				<div className="row">
+					<div className="col-1-of-2">
+						<div className="footer__navigation">
+							<ul className="footer__list">
+								<li className="footer__item">
+									<a href="#" className="footer__link">
+										Company
+									</a>
+								</li>
+								<li className="footer__item">
+									<a href="#" className="footer__link">
+										Contact us
+									</a>
+								</li>
+								<li className="footer__item">
+									<a href="#" className="footer__link">
+										Careers
+									</a>
+								</li>
+								<li className="footer__item">
+									<a href="#" className="footer__link">
+										Privacy Policy
+									</a>
+								</li>
+								<li className="footer__item">
+									<a href="#" className="footer__link">
+										Terms
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div className="col-1-of-2">
+						<p className="footer__copyright">
+							Built by{' '}
+							<a href="#" className="footer__link">
+								Akinde Emmanuel
+							</a>{' '}
+							on a online course{' '}
+							<a href="#" className="footer__link">
+								Advanced CSS and Sass
+							</a>{' '}
+							Copyright &copy; Jonas.io. Lorem ipsum dolor sit
+							amet consectetur adipisicing elit. Esse vel itaque,
+							placeat obcaecati repudiandae eum fugiat autem quae
+							vitae eius.
+						</p>
+					</div>
+				</div>
+			</footer>
+		);
+	}
+}
+export default Footer;
