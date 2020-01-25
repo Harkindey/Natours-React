1e9a25b5d371a02bbddeee61d817e4e0da0dcf12
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2020-01-25 06:33:21 +0100

    removed log error

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import React, { Component } from 'react';
 import './sass/main.scss';
 //import "./css/icon-font.css";
 import Header from './Components/Header';
-import GridTest from './Components/Grid-Test';
+// import GridTest from './Components/Grid-Test';
 import About from './Components/About';
 import Features from './Components/Features';
 import Tours from './Components/Tours';
 import Stories from './Components/Stories';
-import Booking from "./Components/Booking";
-import Footer from "./Components/Footer";
-import Navigation from "./Components/Navigation";
-import Popup from "./Components/Popup"
+import Booking from './Components/Booking';
+import Footer from './Components/Footer';
+import Navigation from './Components/Navigation';
+import Popup from './Components/Popup';
 
 class App extends Component {
 	render() {
